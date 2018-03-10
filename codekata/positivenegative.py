637bea13b97dbefd0f2ca04dad352dd1ab13277d
Zhakir <37235608+Zhakir@users.noreply.github.com>
2018-03-10 15:34:48 +0530

    now create a positive and negative program

---
+++ codekata/positivenegative.py
@@ -0,0 +1,11 @@
+def main(N):
+    a=N
+    if(a>0):
+        print"Number is POSITIVE"
+    elif(a<0):
+        print"Number is NEGATIVE"
+    elif(a==0):
+        print"Number is ZERO"
+
+if __name__ == '__main__':
+    main(1)
